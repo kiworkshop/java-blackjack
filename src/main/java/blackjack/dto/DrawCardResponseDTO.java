8dab5e02893aa03979a3f4d78119f44a74024ace
Ariel429 <arielkim429@gmail.com>
2021-07-27 17:47:31 +0900

    refactor: 누락된 사항 commit

---
+++ src/main/java/blackjack/dto/DrawCardResponseDTO.java
@@ -0,0 +1,11 @@
+package blackjack.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class DrawCardResponseDTO {
+
+    private final String response;
+}

05c16899f5d6fa7aa4287182fed75fc11a6968ce
Beenie93 <seongbeen93@naver.com>
2021-07-30 01:31:54 +0900

    feat: 카드 점수 enum 속성 getter 테스트 및 기능 구현

---
+++ src/test/java/blackjack/domain/enums/ScoreTest.java
@@ -0,0 +1,38 @@
+package blackjack.domain.enums;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ScoreTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"A, 1", "TWO, 2", "THREE, 3", "FOUR, 4", "FIVE, 5", "SIX, 6", "SEVEN, 7", "EIGHT, 8", "NINE, 9", "TEN, 10", "J, 10", "Q, 10", "K, 10"})
+    @DisplayName("카드의 점수를 반환한다.")
+    void getScore(String scoreName, int expectedScore) {
+        //given
+        Score cardScore = Score.valueOf(scoreName);
+
+        //when
+        int score = cardScore.getScore();
+
+        //then
+        assertThat(score).isEqualTo(expectedScore);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"A, A", "TWO, 2", "THREE, 3", "FOUR, 4", "FIVE, 5", "SIX, 6", "SEVEN, 7", "EIGHT, 8", "NINE, 9", "TEN, 10", "J, J", "Q, Q", "K, K"})
+    @DisplayName("카드의 점수 이름을 반환한다.")
+    void getDenomination(String scoreName, String expectedDenomiantion) {
+        //given
+        Score score = Score.valueOf(scoreName);
+
+        //when
+        String denomination = score.getDenomination();
+
+        //then
+        assertThat(denomination).isEqualTo(expectedDenomiantion);
+    }
+}

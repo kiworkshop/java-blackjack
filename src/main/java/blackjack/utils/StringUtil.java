1d1cce259d6be2f30c32cb8a7a6779b7e80470dd
Ariel429 <arielkim429@gmail.com>
2021-07-29 22:37:57 +0900

    refactor: 사용자의 카드 입력 예외처리 추가

--- src/main/java/blackjack/utils/StringUtil.java
+++ src/main/java/blackjack/utils/StringUtil.java
@@ -1,11 +1,14 @@
 package blackjack.utils;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StringUtil {
     private static final String COMMA = ",";
+    private static final String CHECK_ANSWER_Y_OR_N = "y 혹은 n 만 입력할 수 있습니다.";
 
     public static List<String> splitByComma(String input) {
         List<String> names = Arrays.asList(input.split(COMMA));
@@ -15,8 +18,11 @@ public class StringUtil {
     }
 
     public static String validateYesOrNo(String response) {
+        if (StringUtils.isBlank(response)) {
+            throw new IllegalArgumentException(CHECK_ANSWER_Y_OR_N);
+        }
         if (!(response.equals("Y") || response.equals("y") || response.equals("N") || response.equals("n"))) {
-            throw new IllegalArgumentException("y 혹은 n 만 입력할 수 있습니다.");
+            throw new IllegalArgumentException(CHECK_ANSWER_Y_OR_N);
         }
 
         return response;

506fb75eae9433c8e1ca4b2b207fad518236ad0f
Beenie93 <seongbeen93@naver.com>
2021-08-03 16:33:59 +0900

    feat: 딜러, 플레이어들이 처음 받은 카드 2장을 출력하는 기능 추가

--- src/main/java/blackjack/controller/BlackjackController.java
+++ src/main/java/blackjack/controller/BlackjackController.java
@@ -1,6 +1,7 @@
 package blackjack.controller;
 
 import blackjack.domain.GameSystem;
+import blackjack.domain.dto.FirstTwoCardsResponse;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
 
@@ -19,5 +20,7 @@ public class BlackjackController {
         List<String> playerNames = inputView.getPlayerNames();
         GameSystem gameSystem = new GameSystem(playerNames);
         outputView.printGameStart(gameSystem.getPlayerNames());
+        FirstTwoCardsResponse firstTwoCardsResponse = new FirstTwoCardsResponse(gameSystem.getDealerCards(), gameSystem.getPlayerCards());
+        outputView.printFirstTwoCards(gameSystem.getPlayerNames(), firstTwoCardsResponse);
     }
 }
--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,12 +1,37 @@
 package blackjack.view;
 
+import blackjack.domain.dto.CardResponse;
+import blackjack.domain.dto.FirstTwoCardsResponse;
+
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class OutputView {
-
+    private static final String DEALER = "딜러";
+    private static final String SEPARATOR = ", ";
 
     public void printGameStart(List<String> playerNames) {
-        String names = String.join(", ", playerNames);
-        System.out.printf("딜러와 %s에게 2장의 카드를 나누었습니다.", names);
+        String names = String.join(SEPARATOR, playerNames);
+        System.out.printf("%s와 %s에게 2장의 카드를 나누었습니다.%n", DEALER, names);
+    }
+
+    public void printFirstTwoCards(List<String> playerNames, FirstTwoCardsResponse firstTwoCardsResponse) {
+        System.out.printf("%s: %s%n", DEALER, combine(firstTwoCardsResponse.getDealerCards().get(0)));
+
+        IntStream.range(0, playerNames.size())
+                .forEach(i -> System.out.printf("%s카드: %s%n", playerNames.get(i), playerCards(firstTwoCardsResponse.getAllPlayerCards().get(i))));
+    }
+
+    private String combine(final CardResponse cardResponse) {
+        return cardResponse.getDenomination() + cardResponse.getSuit();
+    }
+
+    private String playerCards(final List<CardResponse> cardResponses) {
+        List<String> fullCardNames = cardResponses.stream()
+                .map(this::combine)
+                .collect(Collectors.toList());
+
+        return String.join(SEPARATOR, fullCardNames);
     }
 }
